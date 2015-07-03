f33ae4a68a33b6a8f78b9efa19cf3a5335768edd
Matthew Abner <faridikim@gmail.com>
2015-07-04 00:25:26 +0300

    Chapter 1 - Ricks Guitar App - Final

---
+++ ooad/src/main/java/com/hh/ooad/c01d/finale/Type.java
@@ -0,0 +1,14 @@
+package com.hh.ooad.c01d.finale;
+
+public enum Type { 
+
+  ACOUSTIC, ELECTRIC;
+
+  public String toString() {
+    switch(this) {
+      case ACOUSTIC: return "acoustic";
+      case ELECTRIC: return "electric";
+      default:       return "unspecified";
+    }
+  }
+}
