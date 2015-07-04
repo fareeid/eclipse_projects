ab4884f1cdac8c069067df6c15d6c2b66e85809d
Matthew Abner <faridikim@gmail.com>
2015-07-04 11:51:36 +0300

    Chapter 2 - Doug's Door App - Start

---
+++ ooad/src/main/java/com/hh/ooad/c02a/start/DogDoorSimulator.java
@@ -0,0 +1,21 @@
+package com.hh.ooad.c02a.start;
+
+public class DogDoorSimulator {
+
+  public static void main(String[] args) {
+    DogDoor door = new DogDoor();
+    Remote remote = new Remote(door);
+
+    System.out.println("Fido barks to go outside...");
+    remote.pressButton();
+
+    System.out.println("\nFido has gone outside...");
+    remote.pressButton();
+
+    System.out.println("\nFido's all done...");
+    remote.pressButton();
+
+    System.out.println("\nFido's back inside...");
+    remote.pressButton();
+  }
+}
